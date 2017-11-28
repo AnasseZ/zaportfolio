c3061867aed134fd8e60e0590f55bf324f8ba27a
Anasse Zougarh <zougarh.anasse@gmail.com>
2017-11-28 21:19:20 +0100

    changement du background

--- src/components/Competence.js
+++ src/components/Competence.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Competence = ({ intitule, description }) => (
-  <div className="col-md-3 col-md-offset-1 block">
+  <div className="col-md-4 block">
     <span className="fa-stack fa-lg fa-5x">
       <i className="fa fa-square-o fa-stack-2x" />
       <i className="fa fa-plane fa-stack-1x" />
